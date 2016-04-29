b9237759ecf7f01f2cfc7bc252f95bd73bc035eb
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-04-29 10:23:49 +0200

    snackbar menu

--- js/init.js
+++ js/init.js
@@ -2,21 +2,13 @@
 {
   ML.apiRoot = document.location.hostname.replace('app.', 'api.');
 
-  /*document.getElementById('btn-logout').onclick = function ()
-  {
-    ML.api('auth', 'logout', null, function ()
-    {
-      hasher.setHash('auth/login');
-    });
-  };*/
-
+  // === LOGIN ===
   PP.onKey('#page-login .username', 13, document.querySelector('#page-login .password').focus);
   PP.onKey('#page-login .password', 13, ML.loginImap);
-
   document.querySelector('#page-login .google').onclick = ML.googleStart;
   document.querySelector('#page-login .login').onclick = ML.loginImap;
-  //document.getElementById('btn-contacts').onclick = function () { hasher.setHash('contacts'); };
 
+  // === FILTERS ===
   document.querySelector('#page-contacts .filter').onkeyup = function ()
   {
     var filter = this.value.toUpperCase();
@@ -40,7 +32,25 @@
     });
   };
 
-  function showTags()
+  // === SNACKBAR ===
+  document.querySelector('#snackbar .icon.more').onclick = function ()
+  {
+    var menu = document.getElementById('snackbar-menu-more');
+    if (this.classList.contains('toggled'))
+    {
+      this.className = this.className.replace(/\btoggled\b/, '');
+      menu.style.display = 'none';
+    }
+    else
+    {
+      this.className += ' toggled';
+      menu.style.display = 'block';
+    }
+  };
+
+  // === COMPOSER ===
+  var composerText = document.querySelector('#composer textarea');
+  composerText.onclick = composerText.onfocus = function ()
   {
     var tagsBar = document.querySelector('#composer .tags');
 
@@ -49,10 +59,7 @@
       Array.prototype.forEach.call(document.querySelectorAll('#composer .tag'), function (el) { el.classList.remove('sel'); });
     }
     tagsBar.style.display = 'block';
-  }
-
-  var composerText = document.querySelector('#composer textarea');
-  composerText.onclick = composerText.onfocus = showTags;
+  };
   composerText.onkeydown = function (e)
   {
     if (e.keyCode == 27)
@@ -78,7 +85,7 @@
     };
   });
 
-  // setup path dispatcher
+  // PATHS
   crossroads.addRoute('auth/login', ML.showLogin);
   crossroads.addRoute('contacts', ML.showContacts);
   crossroads.addRoute('chat/{email}', function (email)
