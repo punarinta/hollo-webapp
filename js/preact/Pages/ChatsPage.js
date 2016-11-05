c7810cc785c7d97a95583acaddd2d1ad1378df80
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-11-05 23:40:05 +0100

    pull for chat page

--- js/preact/Pages/ChatsPage.js
+++ js/preact/Pages/ChatsPage.js
@@ -7,6 +7,7 @@ class ChatsPage extends Component
     this.state.pageStart = 0;
     this.state.pageLength = Math.ceil(2.5 * (screen.height - 176) / 72);
     this.state.chats = [];
+    this.state.blockSwipe = 0;
   }
 
   componentDidMount()
@@ -19,18 +20,52 @@ class ChatsPage extends Component
     });
   }
 
+  touchStart(e)
+  {
+    this.pull = 0;
+    this.startY = e.changedTouches[0].pageY;
+    this.ul = this.base.querySelector('ul');
+  }
+
+  touchMove(e)
+  {
+    let distY = e.changedTouches[0].pageY - this.startY;
+
+    if (Math.abs(distY) > 16)
+    {
+      this.setState({blockSwipe: true});
+
+      if (distY > 72)
+      {
+        this.pull = 1;
+        this.ul.style.transform = `translateY(${Math.min(distY, 216)}px)`;
+      }
+    }
+  }
+
+  touchEnd()
+  {
+    this.setState({blockSwipe: false});
+    this.ul.style.transform = 'translateY(0)';
+
+    if (this.pull)
+    {
+      //CO.show(20);
+    }
+  }
+
   render()
   {
     let chats = [];
 
     for (let i in this.state.chats)
     {
-      chats.push(h(ChatRow, {chat: this.state.chats[i]}))
+      chats.push(h(ChatRow, {chat: this.state.chats[i], canSwipe: !this.state.blockSwipe}))
     }
 
     return (
 
-      h('chats-page', null,
+      h('chats-page', {ontouchstart: this.touchStart.bind(this), ontouchmove: this.touchMove.bind(this), ontouchend: this.touchEnd.bind(this)},
         h('ul', null,
           chats
         )
