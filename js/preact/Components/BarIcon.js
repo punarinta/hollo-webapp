554806abccf3ce033d58fe86be3bc28e0fc7b538
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-11-06 11:20:34 +0100

    BarIcon fixes

--- js/preact/Components/BarIcon.js
+++ js/preact/Components/BarIcon.js
@@ -1,18 +1,10 @@
 class BarIcon extends Component
 {
-  onClick()
-  {
-    if (this.props.onclick)
-    {
-      this.props.onclick();
-    }
-  }
-
   render(props)
   {
     return (
 
-      h('bar-icon', {onclick: this.onClick.bind(this)},
+      h('bar-icon', {onclick: this.props.onclick},
         h('svg', {style: {backgroundImage: `url('/gfx/${props.img}')`, width: props.width, height: props.height }}),
         h('div', null,
           props.caption
