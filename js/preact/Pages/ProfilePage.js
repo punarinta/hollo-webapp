c1c9f23c4fb5a2108726925590d903c73077f164
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-11-08 16:55:41 +0100

    checkbox exps

--- js/preact/Pages/ProfilePage.js
+++ js/preact/Pages/ProfilePage.js
@@ -2,10 +2,27 @@ class ProfilePage extends Component
 {
   render()
   {
+    let user = this.props.user;
+
     return (
 
       h('profile-page', null,
-
+        h('div', null,
+          h(Avatar, {user: user}),
+          h('div', null,
+            h('div', null,
+              user.name
+            ),
+            h('div', null,
+              user.email
+            )
+          ),
+          h('button', {onclick: () => ML.go('auth/logout')}, 'Logout')
+        ),
+        h('div', null, 'Settings'),
+        h('ul', null,
+          h(CheckBox, {caption: 'Emojis replace words', checked: CFG.hungryEmojis})
+        ),
         h(BottomBar, null,
           h(BarIcon, {caption: 'Profile', img: 'white/profile_new.svg'}),
           h(BarIcon, {caption: 'Hollo`d', img: 'white/email_new.svg', onclick: () => ML.go('chats')}),
