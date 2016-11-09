bc1adad1d469bc826bc7ec99497937b2c37194eb
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-11-09 19:20:02 +0100

    size control

--- js/preact/Components/BarIcon.js
+++ js/preact/Components/BarIcon.js
@@ -6,8 +6,8 @@ class BarIcon extends Component
 
     return (
 
-      h('bar-icon', {onclick: this.props.onclick},
-        h('svg', {style: {backgroundImage: `url('/gfx/${props.img}.svg')`, width: props.width, height: props.height }}),
+      h('bar-icon', {onclick: this.props.onclick, style: this.props.fullHeight ? {height: props.height} : {}},
+        h('svg', {style: {backgroundImage: `url('/gfx/${props.img}.svg')`, width: props.width || '20px', height: props.height || '20px' }}),
         caption
       )
     );
