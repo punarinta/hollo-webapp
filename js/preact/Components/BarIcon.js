ba20ffe75e1ed5034a3ac8f5ec88b216337b068c
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-11-09 10:29:51 +0100

    support captionless buttons

--- js/preact/Components/BarIcon.js
+++ js/preact/Components/BarIcon.js
@@ -2,13 +2,13 @@ class BarIcon extends Component
 {
   render(props)
   {
+    let caption = props.caption.length ? h('div', null, props.caption) : null;
+
     return (
 
       h('bar-icon', {onclick: this.props.onclick},
         h('svg', {style: {backgroundImage: `url('/gfx/${props.img}')`, width: props.width, height: props.height }}),
-        h('div', null,
-          props.caption
-        )
+        caption
       )
     );
   }
