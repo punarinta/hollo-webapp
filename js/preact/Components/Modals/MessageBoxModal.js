246a87d0d199b169bc2b435e5276d51f07ccaaad
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-11-11 22:47:14 +0100

    support input mboxes

--- js/preact/Components/Modals/MessageBoxModal.js
+++ js/preact/Components/Modals/MessageBoxModal.js
@@ -1,24 +1,29 @@
 class MessageBoxModal extends Component
 {
-  onClick()
+  onClick(code)
   {
-    if (typeof this.props.data.cb == 'function') this.props.data.cb();
+    let payload = null;
+
+    if (typeof this.props.data.input != 'undefined') payload = this.base.querySelector('input').value;
+
+    if (typeof this.props.data.cb == 'function') this.props.data.cb(code, payload);
     if (typeof this.props.onclose == 'function') this.props.onclose()
   }
 
   render()
   {
     let data = this.props.data,
-        buttons = [h('button', {className: 'btn ok', onclick: this.onClick.bind(this)}, 'OK')];
+        buttons = [h('button', {className: 'btn ok', onclick: () => this.onClick(1)}, 'OK')];
 
     if (data.type == 1)
     {
-      buttons.push(h('button', {className: 'btn cancel', onclick: this.onClick.bind(this)}, 'Cancel'))
+      buttons.push(h('button', {className: 'btn cancel', onclick: () => this.onClick(0)}, 'Cancel'))
     }
 
     return h('message-box-modal', null,
       h('div', null,
         h(MessageBody, {html: data.html}),
+        typeof data.input == 'undefined' ? '' : h('input', {value: data.input, type: 'text', autofocus: 'autofocus'}),
         h('div', {className: 'foot'},
           buttons
         )
