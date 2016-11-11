902e7d54655b781b447b0804b65e25b865d017ab
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-11-11 16:34:15 +0100

    allo custom classes

--- js/preact/Components/BarIcon.js
+++ js/preact/Components/BarIcon.js
@@ -6,7 +6,7 @@ class BarIcon extends Component
 
     return (
 
-      h('bar-icon', {onclick: this.props.onclick, style: this.props.fullHeight ? {height: props.height} : {}},
+      h('bar-icon', {className: this.props.className, onclick: this.props.onclick, style: this.props.fullHeight ? {height: props.height} : {}},
         h('svg', {style: {backgroundImage: `url('/gfx/${props.img}.svg')`, width: props.width || '20px', height: props.height || '20px' }}),
         caption
       )
