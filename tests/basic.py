d867637c4608506868ba2de4ecf3cfe8386b1647
punarinta <cheaterx@yandex.ru>
2016-11-16 21:14:01 +0100

    test setup

--- tests/basic.py
+++ tests/basic.py
@@ -1,6 +1,7 @@
 # https://christopher.su/2015/selenium-chromedriver-ubuntu/
 
 import re
+import config
 from selenium import webdriver
 from time import sleep
 
@@ -13,8 +14,8 @@ browser.get(rootURL)
 
 username = browser.find_element_by_xpath("//input[@type='email']")
 password = browser.find_element_by_xpath("//input[@type='password']")
-username.send_keys('******')
-password.send_keys('******')
+username.send_keys(config.username)
+password.send_keys(config.password)
 
 login_button = browser.find_element_by_xpath('//button[@class="login"]')
 login_button.click()
