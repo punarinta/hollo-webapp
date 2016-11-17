513beca6e570071cfcef364ddbe9df4e4fba87e7
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-11-17 09:09:42 +0100

    use css selectors, add init path check, init position

--- tests/basic.py
+++ tests/basic.py
@@ -11,28 +11,33 @@ def log(text, status):
 	else:
 		print '\033[91m[FAILED]\033[0m ' + text
 
+def waitForElement ():
+    return
+
 # remember to input your own file path to your chrome driver here if you're using chrome
 browser = webdriver.Chrome()
-browser.set_window_size(414, 736)	# 375 x 667
+browser.set_window_size(414, 736)	    # 375 x 667
+browser.set_window_position(610, 0)     # 1024 - W x 0
 
 rootURL = 'https://app.hollo.dev'
 browser.get(rootURL)
+log('Path init to /auth/login', browser.current_url == rootURL + '/auth/login')
 
-username = browser.find_element_by_xpath("//input[@type='email']")
-password = browser.find_element_by_xpath("//input[@type='password']")
-username.send_keys(config.username)
-password.send_keys(config.password)
+i_username = browser.find_element_by_css_selector("input[type='email']")
+i_password = browser.find_element_by_css_selector("input[type='password']")
+i_username.send_keys(config.username)
+i_password.send_keys(config.password)
 
-login_button = browser.find_element_by_xpath('//button[@class="login"]')
-login_button.click()
+b_login = browser.find_element_by_css_selector('button.login')
+b_login.click()
 sleep(4.0)
 log('Path changed to /chats', browser.current_url == rootURL + '/chats')
 
-profile_button = browser.find_element_by_xpath('//chats-page/bottom-bar/bar-icon[1]')
-profile_button.click()
+b_profile = browser.find_element_by_css_selector('chats-page bottom-bar bar-icon:nth-child(1)')
+b_profile.click()
 sleep(2.0)
 log('Path changed to /profile', browser.current_url == rootURL + '/profile')
 
 sleep(0.5)
 print ''
-# browser.quit()
+browser.quit()
