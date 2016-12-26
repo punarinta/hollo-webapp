ac6cc9331089021209721aa72de6c4a261e1c91e
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-12-26 13:07:48 +0100

    optimize rendering of pickers' lists

--- js/Components/Modals/UserPickerModal.js
+++ js/Components/Modals/UserPickerModal.js
@@ -6,18 +6,23 @@ class UserPickerModal extends Component
     this.filterTimer = null;
     this.emailFilter = '';
     this.state.users = [];
+    this.displayPerScreen = ((screen.height - 160) / 32) + 1;
+    this.state.maxDisplay = this.displayPerScreen;
   }
 
   componentDidMount()
   {
+    this.scrollReference = this.scroll.bind(this);
     this.usersUpdateReference = this.usersUpdate.bind(this);
     this.chatsUpdateReference = this.chatsUpdate.bind(this);
+    this.base.querySelector('ul').addEventListener('scroll', this.scrollReference);
     window.addEventListener('hollo:users:update', this.usersUpdateReference);
     window.addEventListener('hollo:chat:update', this.chatsUpdateReference);
   }
 
   componentWillUnmount()
   {
+    this.base.querySelector('ul').removeEventListener('scroll', this.scrollReference);
     window.removeEventListener('hollo:users:update', this.usersUpdateReference);
     window.addEventListener('hollo:chat:update', this.chatsUpdateReference);
   }
@@ -60,35 +65,53 @@ class UserPickerModal extends Component
     if (typeof this.props.data.onselect == 'function') this.props.data.onselect(user)
   }
 
+  scroll()
+  {
+    let el = this.base.querySelector('li:nth-last-child(2)');
+
+    if (el && el.getBoundingClientRect().bottom < screen.height + 50)
+    {
+      this.setState({maxDisplay: this.state.maxDisplay + this.displayPerScreen});
+    }
+  }
+
   render(props)
   {
-    if (!props.data) return h('user-picker-modal', {style: {display: 'none'}});
+    if (!props.data) return h('user-picker-modal', {style: {display: 'none'}}, h('div', null, h('ul')));
 
-    let items = [];
+    let items = [], count = 0;
 
     if (props.data.chatMode)
     {
       for (let i in this.state.chats)
       {
+        if (count > this.state.maxDisplay) break;
+
         let chat = this.state.chats[i],
             name = ML.xname(chat);
 
         items.push(h('li', { onclick: () => this.onSelect(chat) },
           h(Avatar, {size: 32, chat}),
           h('div', null, name[0])
-        ))
+        ));
+
+        ++count;
       }
     }
     else
     {
       for (let i in this.state.users)
       {
+        if (count > this.state.maxDisplay) break;
+
         let user = this.state.users[i];
 
         items.push(h('li', { onclick: () => this.onSelect(user) },
           h(Avatar, {size: 32, user}),
           h('div', null, user.name || user.email)
-        ))
+        ));
+
+        ++count;
       }
     }
 
