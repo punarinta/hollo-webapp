b18b8e3b5111cfe823cee0186e5bf1d1386eed67
punarinta <cheaterx@yandex.ru>
2017-01-02 19:50:05 +0100

    French translation, smallfixes

---
+++ js/translations/fr-FR.js
@@ -0,0 +1,70 @@
+$locales['fr-FR'] =
+{
+  BTN_PROFILE       : 'Profil',
+  BTN_INBOX         : 'Boîte mail',
+  BTN_MUTED         : 'Silencieux',
+  BTN_OK            : 'OK',
+  BTN_CANCEL        : 'Annuler',
+  BTN_LOGOUT        : 'Déconnexion',
+  BTN_TESTPUSH      : 'Notification d\'essai',
+  BTN_ADD_USERS     : 'Ajouter à',
+  BTN_ADD_SUBJ      : 'Nouvel Objet',
+  BTN_SIGN_IN       : 'connectez-vous ',
+  BTN_GOOGLE        : 'connectez-vous avec Google',
+
+  CHAT_MUTE         : 'Silencieux',
+  CHAT_UNMUTE       : 'Activé',
+  CHAT_READ         : 'Marquer\ncomme lu',
+  CHAT_UNREAD       : 'Marquer\ncomme non lu',
+  CHAT_RENAME       : 'Renommer le chat',
+  CHAT_LEAVE        : 'Quitter le chat',
+  CHAT_CLEAR        : 'Effacer les notes',
+
+  MSG_FORWARD       : 'Transférer...',
+  MSG_SHOW_ORIG     : 'Afficher l\'email original',
+  MSG_SHOW_HTML     : 'Afficher la version HTML',
+
+  CAP_MY_NOTES      : 'Mes notes',
+  CAP_NEW_CHAT      : 'Créer un chat avec %s',
+  CAP_WRITE_NEW     : 'Rédiger un nouveau hollo...',
+  CAP_SUBJ_FLT      : 'Filtre par objet',
+  CAP_NEW_SUBJ      : 'Nouvel objet',
+  CAP_NEW_NAME      : 'Nouveau nom:',
+  CAP_SIGN_IN       : 'Connexion',
+  CAP_EMAIL         : 'Email',
+  CAP_PASSWORD      : 'Mot de passe',
+
+  HINT_FLT_ON       : 'Pour lancer une nouv,\nsaisissez une adresse e-mail.',
+  HINT_SYNCING      : 'Bienvenue sur Hollo!\nVeuillez attendre jusqu\'à ce que\nvos messages se téléchargent',
+  HINT_NEW_CHAT     : 'Vous voulez créer une nouvelle discussion?\nEntrez l\'adresse e-mail du destinataire',
+  HINT_NO_MSGS      : 'Il n\'ya aucun message sur ce',
+  HINT_NO_NOTES     : 'Ici, vous pouvez ajouter vos propres notes\ncomme créer vos listes d\'achats\nou un rappel pour répondre à un e-mail',
+  HINT_NO_FILES     : 'Aucun fichiers joints à ce chat',
+  HINT_NO_SUBJS     : 'Pas de sujets dans ce chat',
+  HINT_AREYOUSURE   : 'Etes-vous sûr?',
+  HINT_CHAT_SEARCH  : 'Rechercher un chat ou démarrer un nouveau',
+  HINT_FORWARDED    : 'Message transmis',
+
+  ERR_BIG_FILE      : 'Taille maximale du fichier est de 10 Mo.',
+  ERR_NO_ORIG       : 'Ce message n\'est pas dans votre boîte de réception.',
+
+  SYS_SETTINGS      : 'Paramètres',
+  SYS_EMOJIS        : 'Emojis remplacent les mots',
+  SYS_AVATARS       : 'Avatars de couleur',
+  SYS_NOTES         : 'Voir mes notes',
+  SYS_PUSHINFO      : 'La notification sera livrée en ~ 5 secondes.\nOk pour envoyer au mobile. Annuler pour envoyer au bureau',
+
+  CAL_OPENLINK      : 'Ouvrir le calendrier',
+  CAL_YOU_ACCEPT    : '✔ vous avez accepté l\'invitation',
+  CAL_ACCEPT        : '✔ %s a accepté  l\'invitation',
+  CAL_YOU_TENT      : '❓ Vous avez répondu peut-être à l\'invitation',
+  CAL_TENT          : '❓ %s a répondu peut-être à l\'invitation',
+
+  self: 'français'
+};
+
+$locales['fr-BE'] = $locales['fr-FR'];
+$locales['fr-CA'] = $locales['fr-FR'];
+$locales['fr-CH'] = $locales['fr-FR'];
+$locales['fr-LU'] = $locales['fr-FR'];
+$locales['fr-MC'] = $locales['fr-FR'];
