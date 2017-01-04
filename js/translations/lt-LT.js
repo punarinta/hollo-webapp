94c2f3d3fedf9e5577d416592fa1a18cc01c8dce
Vladimir Osipov <vladimir.osipov@coursio.com>
2017-01-04 21:19:40 +0100

    spelling

---
+++ js/translations/lt-LT.js
@@ -0,0 +1,77 @@
+$locales['en-US'] =
+{
+  BTN_PROFILE       : 'Nustatymai',
+  BTN_INBOX         : 'Svarbu',
+  BTN_MUTED         : 'Nesvarbu',
+  BTN_OK            : 'OK',
+  BTN_CANCEL        : 'Atšaukti',
+  BTN_LOGOUT        : 'Atsijungti',
+  //BTN_TESTPUSH      : 'Test notifications',
+  BTN_ADD_USERS     : 'Pridėti naudotoją',
+  BTN_ADD_SUBJ      : 'Nauja Tema',
+  BTN_SIGN_IN       : 'prisijungti',
+  BTN_GOOGLE        : 'Prisijungti nauduojant Google',
+
+  HI_WELCOME        : 'Sveiki atvykę į Hollo',
+  HI_GIMME_5        : 'Prašome kelių sekundžių konfiguoroti paskyrą',
+  HI_INBOX          : 'Emails from people, important stuff.',
+  HI_MUTED          : 'Emails from robots, or others that\nyou don’t want notifications about.',
+  HI_SYNCING        : 'Syncing your emails',
+  HI_SYNCED         : 'Let’s go!',
+
+  CHAT_MUTE         : 'Mute',
+  CHAT_UNMUTE       : 'Unmute',
+  CHAT_READ         : 'Mark as\nread',
+  CHAT_UNREAD       : 'Mark as\nunread',
+  CHAT_RENAME       : 'Rename chat',
+  CHAT_LEAVE        : 'Leave chat',
+  CHAT_CLEAR        : 'Clear notes',
+
+  MSG_FORWARD       : 'Forward message...',
+  MSG_SHOW_ORIG     : 'Show original email',
+  MSG_SHOW_HTML     : 'Show HTML version',
+
+  QS_N_UNREAD       : '%s unread',
+  QS_N_LEFT         : '%s to go',
+  QS_READ           : 'Read',
+  QS_REPLY          : 'Reply',
+  QS_SKIP           : 'Skip',
+
+  CAP_MY_NOTES      : 'My notes',
+  CAP_NEW_CHAT      : 'Create a chat with %s',
+  CAP_WRITE_NEW     : 'Write a new hollo...',
+  CAP_SUBJ_FLT      : 'Filtering by subject',
+  CAP_NEW_SUBJ      : 'New subject',
+  CAP_NEW_NAME      : 'Enter new name:',
+  CAP_SIGN_IN       : 'Sign in',
+  CAP_EMAIL         : 'Email',
+  CAP_PASSWORD      : 'Password',
+
+  HINT_FLT_ON       : 'To start a new\nconversation type in\nan email address.',
+  HINT_SYNCING      : 'Welcome to Hollo!\nWait a bit please until\nyour messages are fetched...',
+  HINT_NEW_CHAT     : 'Wanna create a new chat?\nJust type in an email!',
+  HINT_NO_MSGS      : 'No messages yet\nOr they became too old',
+  HINT_NO_NOTES     : 'Feel free to talk to yourself here,\nmaybe you need to do something?\nOr write down a shopping list?',
+  HINT_NO_FILES     : 'No files in this chat',
+  HINT_NO_SUBJS     : 'No subjects in this chat',
+  HINT_AREYOUSURE   : 'Are you sure?',
+  HINT_CHAT_SEARCH  : 'Search chat or start new',
+  HINT_FORWARDED    : 'Forwarded message',
+
+  ERR_BIG_FILE      : 'Sorry, maximum attachment size is 10MB.',
+  ERR_NO_ORIG       : 'We are sorry, but this message is absent in your mailbox.',
+
+  SYS_SETTINGS      : 'Settings',
+  SYS_EMOJIS        : 'Emojis replace words',
+  SYS_AVATARS       : 'Colored avatars',
+  SYS_NOTES         : 'Show local notes',
+  SYS_PUSHINFO      : 'Notification will be delivered in ~5 seconds. OK to test mobile push, Cancel to test desktop push.',
+
+  CAL_OPENLINK      : 'Open in calendar',
+  CAL_YOU_ACCEPT    : '✔ ️you accepted this invite',
+  CAL_ACCEPT        : '✔ %s accepted this invite',
+  CAL_YOU_TENT      : '✔ ️you accepted this invite',
+  CAL_TENT          : '❓ %s said "maybe" to this invite',
+
+  self: 'English'
+};
