2e8d64146d5658a1f6fc44017e5d4a57c5375d5f
Vladimir Osipov <vladimir.osipov@coursio.com>
2017-01-05 13:26:31 +0100

    prepare locale files

---
+++ js/translations/mzs-X.js
@@ -0,0 +1,17 @@
+$locales['mzs-X'] =
+{
+
+};
+
+$locales['bg-BG'] = $locales['mzs-X'];
+$locales['bs-BA'] = $locales['mzs-X'];
+$locales['cs-CZ'] = $locales['mzs-X'];
+$locales['hr-HR'] = $locales['mzs-X'];
+$locales['mk-MK'] = $locales['mzs-X'];
+$locales['pl-PL'] = $locales['mzs-X'];
+$locales['sr-BA'] = $locales['mzs-X'];
+$locales['sr-ME'] = $locales['mzs-X'];
+$locales['sr-RS'] = $locales['mzs-X'];
+$locales['sk-SK'] = $locales['mzs-X'];
+$locales['sl-SI'] = $locales['mzs-X'];
+$locales['hsb-DE'] = $locales['mzs-X'];
