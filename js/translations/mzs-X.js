6a2a213ea2457875312779b894b4e76adc461fc7
Vladimir Osipov <vladimir.osipov@coursio.com>
2017-01-05 17:09:42 +0100

    Interslavic translation

--- js/translations/mzs-X.js
+++ js/translations/mzs-X.js
@@ -2,7 +2,80 @@
 
 $locales['mzs-X'] =
 {
+  BTN_PROFILE       : 'Nastavke',
+  BTN_INBOX         : 'Važne',
+  BTN_MUTED         : 'Nevažne',
+  BTN_OK            : 'OK',
+  BTN_CANCEL        : 'Anulovati',
+  BTN_LOGOUT        : 'Odlogovati sę',
+  BTN_TESTPUSH      : 'Izpytati oglåšenja',
+  BTN_ADD_USERS     : 'Dodati',
+  BTN_ADD_SUBJ      : 'Novy Prědmet',
+  BTN_SIGN_IN       : 'logovati sę',
+  BTN_GOOGLE        : 'logovati sę Google\'om',
 
+  HI_WELCOME        : 'Vitajte v Hollo',
+  HI_GIMME_5        : 'Dajte nam nekoliko sekund žeby nastaviti konto',
+  HI_INBOX          : 'Piśma od ljudi, važne poslanja.',
+  HI_MUTED          : 'Piśma od robotov abo to, ob čem\nne hćete prijmati oglåšenja.',
+  HI_SYNCING        : 'Piśma sinhronizujųt sę',
+  HI_SYNCED         : 'Pojdimo!',
+
+  CHAT_MUTE         : 'V nevažne',
+  CHAT_UNMUTE       : 'V važne',
+  CHAT_READ         : 'Označiti ako\npročitano',
+  CHAT_UNREAD       : 'Označiti ako\nnepročitano',
+  CHAT_RENAME       : 'Prěimenovati čat',
+  CHAT_LEAVE        : 'Ostaviti čat',
+  CHAT_CLEAR        : 'Izčistiti zapiski',
+
+  MSG_FORWARD       : 'Prěslati poslanje...',
+  MSG_SHOW_ORIG     : 'Pokazati pŕvonačaľnų formų',
+  MSG_SHOW_HTML     : 'Pokazati HTML-versijų',
+
+  QS_N_UNREAD       : '%s novyh',
+  QS_N_LEFT         : 'ješče %s',
+  QS_READ           : 'Gotovo',
+  QS_REPLY          : 'Odpovědati',
+  QS_SKIP           : 'Nastųpno',
+
+  CAP_MY_NOTES      : 'Moje zapiski',
+  CAP_NEW_CHAT      : 'Stvoriti čat s %s',
+  CAP_WRITE_NEW     : 'Napišite čto-nebųď...',
+  CAP_SUBJ_FLT      : 'Filtruje prědmetom',
+  CAP_NEW_SUBJ      : 'Novy prědmet',
+  CAP_NEW_NAME      : 'Novo imę:',
+  CAP_SIGN_IN       : 'Logovati sę',
+  CAP_EMAIL         : 'E-poštovy adres',
+  CAP_PASSWORD      : 'Parola',
+
+  HINT_FLT_ON       : 'Že by načęti novy čat\nnapišite e-poštovy adres.',
+  HINT_SYNCING      : 'Vitajte v Hollo!\nPočěkajte poki ide\nsinhronizacija...',
+  HINT_NEW_CHAT     : 'Hćete stvoriti novy čat?\nProsto napišite e-poštovy adres.',
+  HINT_NO_MSGS      : 'Ne sųt poslań v sem čatě.\nIli one sųt postarěli.',
+  HINT_NO_NOTES     : 'Ostavjajte tu svoje zapiski.\nNapriklad, spisy zakupov.',
+  HINT_NO_FILES     : 'Sej čat ne imaje fajlov',
+  HINT_NO_SUBJS     : 'Sej čat ne imaje prědmetov',
+  HINT_AREYOUSURE   : 'Uvěrjeni?',
+  HINT_CHAT_SEARCH  : 'Najdti čat ili stvoriti novy',
+  HINT_FORWARDED    : 'Prěslano poslanje',
+
+  ERR_BIG_FILE      : 'Žaľ, ale nemožno dodati fail vęče 10 MB.',
+  ERR_NO_ORIG       : 'Žaľ, ale se poslanje už ne istnuje.',
+
+  SYS_SETTINGS      : 'Nastavke',
+  SYS_EMOJIS        : 'Emodži råzměnjajųt slova',
+  SYS_AVATARS       : 'Barvne avatary',
+  SYS_NOTES         : 'Pokazyvati zapiski',
+  SYS_PUSHINFO      : 'Oglåšenje dojde za 5 sekund. OK — izpytati na telefoně, Anulovati — na desktopě.',
+
+  CAL_OPENLINK      : 'Odkryti v kalendari',
+  CAL_YOU_ACCEPT    : '✔ jeste prijęli pozvanje',
+  CAL_ACCEPT        : '✔ %s jest prijęl pozvanje',
+  CAL_YOU_TENT      : '✔ jeste izrěkli "može"',
+  CAL_TENT          : '✔ %s jest izrěkl "može"',
+
+  self: 'Medžuslovjanski'
 };
 
 $locales['bg-BG'] = $locales['mzs-X'];
