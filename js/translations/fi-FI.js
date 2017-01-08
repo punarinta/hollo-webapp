7256c6bc8b9d61b1a10ed5affe3d6eb0ed72ac08
Vladimir Osipov <vladimir.osipov@coursio.com>
2017-01-08 11:50:34 +0100

    add FI and MZS translations

--- js/translations/fi-FI.js
+++ js/translations/fi-FI.js
@@ -1,4 +1,78 @@
 $locales['fi-FI'] =
 {
+  BTN_PROFILE       : 'Asetukset',
+  BTN_INBOX         : 'Kätevät',
+  BTN_MUTED         : 'Hiljaiset',
+  BTN_OK            : 'OK',
+  BTN_CANCEL        : 'Peruuta',
+  BTN_LOGOUT        : 'Kirjaudu ulos',
+  BTN_TESTPUSH      : 'Testaa ilmoitukset',
+  BTN_ADD_USERS     : 'Lisää',
+  BTN_ADD_SUBJ      : 'Uusi Aihe',
+  BTN_SIGN_IN       : 'kirjaudu sisään',
+  BTN_GOOGLE        : 'Kirjaudu sisään Google:n kautta',
 
+  HI_WELCOME        : 'Tervetuloa Holloon',
+  HI_GIMME_5        : 'Sinun tili perustetaan',
+  HI_INBOX          : 'Viestit ihmisiltä, tärkeät postit.',
+  HI_MUTED          : 'Viestit roboteilta tai sellaisia,\njoista et halua saada ilmoituksia',
+  HI_SYNCING        : 'Viestit synkronoidaan',
+  HI_SYNCED         : 'Mennään!',
+
+  CHAT_MUTE         : 'Hiljaisiin',
+  CHAT_UNMUTE       : 'Käteviin',
+  CHAT_READ         : 'Luettuihin',
+  CHAT_UNREAD       : 'Luetuista',
+  CHAT_RENAME       : 'Nimeä juttu',
+  CHAT_LEAVE        : 'Pois jutusta',
+  CHAT_CLEAR        : 'Tyhjennä muistiinpanot',
+
+  MSG_FORWARD       : 'Lähetä edelleen...',
+  MSG_SHOW_ORIG     : 'Näytä alkuperäisteos',
+  MSG_SHOW_HTML     : 'Näytä HTML-versio',
+
+  QS_N_UNREAD       : '%s uutta',
+  QS_N_LEFT         : '%s lisää',
+  QS_READ           : 'Luin',
+  QS_REPLY          : 'Vastaa',
+  QS_SKIP           : 'Hypi yli',
+
+  CAP_MY_NOTES      : 'Omat muistiinpanot',
+  CAP_NEW_CHAT      : 'Luo juttu %s:n kanssa',
+  CAP_WRITE_NEW     : 'Kirjoita jotain...',
+  CAP_SUBJ_FLT      : 'Suodatetaan',
+  CAP_NEW_SUBJ      : 'Uusi aihe',
+  CAP_NEW_NAME      : 'Uusi nimi:',
+  CAP_SIGN_IN       : 'Kirjautuminen',
+  CAP_EMAIL         : 'Sähköpostiosoite',
+  CAP_PASSWORD      : 'Salasana',
+  CAP_TODAY         : 'tänään',
+
+  HINT_FLT_ON       : 'Kirjoita sähköpostiosoite\nuuden keskustelun aloittaaksesi.',
+  HINT_SYNCING      : 'Tervetuloa Holloon\nOdota vähän kun sinun viestit haetaan...',
+  HINT_NEW_CHAT     : 'Haluatko luoda uuden jutun?\nKirjoita vain sähköpostiosoite!',
+  HINT_NO_MSGS      : 'Ei viestejä tässä\nTai ne ovat vanhentuneet',
+  // HINT_NO_NOTES     : 'Feel free to talk to yourself here,\nmaybe you need to do something?\nOr write down a shopping list?',
+  HINT_NO_FILES     : 'Ei tiedostoja tässä jutussa',
+  HINT_NO_SUBJS     : 'Ei aiheita tässä jutussa',
+  HINT_AREYOUSURE   : 'Oletko varma?',
+  HINT_CHAT_SEARCH  : 'Hae jutusta tai luo uuden',
+  HINT_FORWARDED    : 'Edelleenlähetetty viesti',
+
+  ERR_BIG_FILE      : 'Valitettavasti liitteen enimmäiskoko on 10 Mt.',
+  ERR_NO_ORIG       : 'Tätä viestiä ei ole löytynyt postilaatikossasi.',
+
+  SYS_SETTINGS      : 'Asetukset',
+  SYS_EMOJIS        : 'Emojit korvaavat sanot',
+  SYS_AVATARS       : 'Värilliset avatarit',
+  SYS_NOTES         : 'Näytä muistiinpanot',
+  SYS_PUSHINFO      : 'Ilmoitus toimitetaan 5 sekunnissa. Paina ""OK"" testataksesi mobiililla, ""Peruuta"" — desktopilla.',
+
+  CAL_OPENLINK      : 'Avaa kalenterissa',
+  CAL_YOU_ACCEPT    : '✔ hyväksyit tämän kutsun',
+  CAL_ACCEPT        : '✔ hyväksyi tämän kutsun',
+  CAL_YOU_TENT      : '❓ sanoit "ehkä" tässä kutsussa',
+  CAL_TENT          : '❓ sanoi "ehkä" tässä kutsussa',
+
+  self: 'suomi'
 };
